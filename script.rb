e5c000f009029d9800089563368217c45b444200
willofD13 <leonidastsiakas13@gmail.com>
2023-08-16 19:04:19 +0300

    Put the nodes in a hash

--- script.rb
+++ script.rb
@@ -1,8 +1,22 @@
+#+2+1, +2-1, +1+2, +1-2, -1+2, -1-2, -2+1, -2-1
+
 class Chess_board
+    attr_accessor :vertices
 
     def initialize(array)
-        @vertices = array.repeated_permutation(2).to_a
+        nodes = array.repeated_permutation(2).to_a
+        @vertices = {}
+        create_board(nodes)
     end
+
+    def create_board(nodes)
+        nodes.each do |node|
+            vertices[node] = []
+        end
+    end
+
+        
+
 end
 
 board = Chess_board.new([1,2,3,4,5,6,7,8])
