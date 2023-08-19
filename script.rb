70b935fe1a9a279c82c3b7bdc88d4af8e56f7951
willofD13 <leonidastsiakas13@gmail.com>
2023-08-19 15:08:17 +0300

    Turn every array item into node objects

--- script.rb
+++ script.rb
@@ -1,10 +1,27 @@
 #+2+1, +2-1, +1+2, +1-2, -1+2, -1-2, -2+1, -2-1
 
+class Node 
+    attr_accessor :predecessor, :distance, :value
+
+    def initialize(value,predecessor = nil, distance = 0)
+        @value = value
+        @predecessor = predecessor
+        @distance = distance
+    end
+end
+
 class Chess_board
     attr_accessor :vertices
 
     def initialize(array)
         nodes = array.repeated_permutation(2).to_a
+        index = 0
+        
+        while index < nodes.length
+            nodes[index] = Node.new(nodes[index])
+            index += 1
+        end
+        
         @vertices = {}
         create_board(nodes)
     end
@@ -15,18 +32,30 @@ class Chess_board
         end
 
         vertices.each do |k,v|
-            add_neighbors(k,v)
+            add_neighbors(k,v,nodes)
         end
         vertices
     end
 
-    def add_neighbors(k,v)
+    def add_neighbors(k,v,nodes)
         moves = [[+2,+1], [+2,-1], [+1,+2], [+1,-2], [-1,+2], [-1,-2], [-2,+1], [-2,-1]]
 
         moves.each do |move|
-            x = k[0] + move[0]
-            y = k[1] + move[1]
-            v << [x,y] if x.between?(1,8) && y.between?(1,8)
+            x = k.value[0] + move[0]
+            y = k.value[1] + move[1]
+            
+            i = 0
+            while i < nodes.length 
+                if !x.between?(1,8) || !y.between?(1,8)
+                    break
+                elsif nodes[i].value == [x,y]
+                    v << nodes[i]
+                    break
+                else 
+                    i += 1
+                    next
+                end
+            end
         end 
         v 
     end
